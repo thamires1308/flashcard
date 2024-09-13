4fa96c4b9d6833f61b0e98072fe8739fe3a6c2af
thamires1308 <146346645+thamires1308@users.noreply.github.com>
2024-09-13 10:39:26 -0300

    .

---
+++ pergunta.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é programação?',
+    'É o ato da escrita dos códigos entendíveis pelos computadores, permitindo assim que eles realizem as funções pedidas.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'É um conjunto de instruções, como uma receita de bolo, constituído de um número finito de passos'
+)
+
+criaCartao(
+    'Programação',
+    'O que a programação ensina?',
+    'A ter base no raciocínio lógico e por desenvolver soluções para problemas.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a melhor linguagem de programação?',
+    ' C, C++, Python.'
+)
